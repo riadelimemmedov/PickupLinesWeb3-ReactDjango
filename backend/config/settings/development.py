2dce9508aade5915bc2af3f22b45b8d25e38cd30
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-04-21 21:02:51 +0400

    Create seperate development and production file for app

---
+++ backend/config/settings/development.py
@@ -0,0 +1,41 @@
+from ..base import *
+from decouple import config
+
+#!Debug
+DEBUG = True
+
+
+#!Databases
+
+# Default Sqlite For Development
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+}
+
+
+# Migrate Postgress and Docker
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "HOST": config("POSTGRES_HOST"),
+#         "NAME": config("POSTGRES_DB"),
+#         "USER": config("POSTGRES_USER"),
+#         "PASSWORD": config("POSTGRES_PASSWORD"),
+#         "PORT": config("POSTGRES_PORT", 5432),
+#         "DISABLE_SERVER_SIDE_CURSORS": True,
+#     }
+# }
+
+#!Installed Apps
+INSTALLED_APPS += []
+
+
+#!Middleware
+MIDDLEWARE += []
+
+"""
+    These commented config will use when you are runnning the project on Docker
+"""
---
+++ backend/config/settings/production.py
@@ -0,0 +1,14 @@
+from ..base import *
+from decouple import config
+
+#!Debug
+DEBUG = False
+
+
+#!DATABASES
+#?Not Touch this line,because not configutation docker image,use only local
+# DATABASES = {
+#     "default": {
+#       Using docker postgres image
+#     }
+# }
