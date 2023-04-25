aaacd83dd30ac38961c32ae95fa616b9173e8e73
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-04-25 09:37:41 +0400

    Create custom queryset for SoftDeleteion

---
+++ backend/abstract/querysets.py
@@ -0,0 +1,18 @@
+# Django Methods and Function
+from django.db.models.query import QuerySet
+from django.utils import timezone
+
+
+#!SoftDeletionQuerySet
+class SoftDeletionQuerySet(QuerySet):
+    def delete(self):
+        return super(SoftDeletionQuerySet, self).update(deleted_at=timezone.now())
+
+    def hard_delete(self):
+        return super(SoftDeletionQuerySet, self).delete()
+
+    def alive(self):
+        return self.filter(deleted_at=None)
+
+    def dead(self):
+        return self.exclude(deleted_at=None)
