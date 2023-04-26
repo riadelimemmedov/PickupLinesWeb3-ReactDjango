8aed6747eedae17b515818fd38d77ee13ab76039
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-04-26 09:46:05 +0400

    Add User Property to AppName class

--- backend/abstract/apps.py
+++ backend/abstract/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class AbstractConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'abstract'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "abstract"
