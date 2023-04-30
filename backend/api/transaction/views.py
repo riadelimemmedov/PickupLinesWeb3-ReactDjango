33d98e793041bae69936166a259e4298003a5475
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-04-30 08:11:34 +0400

    Is added BlockListRetrieveViewSet and BlockCreateViewSet

--- backend/api/transaction/views.py
+++ backend/api/transaction/views.py
@@ -21,6 +21,9 @@ from rest_framework.permissions import (
 )
 from account.permissions import AdminRequired, ProfileRequired
 
+# Third Party Packages
+import requests
+from decouple import config
 
 # Create your views here.
 
@@ -33,9 +36,10 @@ class TransactionListRetrieveViewSet(viewsets.ViewSet):
     """
 
     queryset = Transaction.objects.get_is_complete()
+    serializer_class = TransactionSerializer
     lookup_field = "pk"
 
-    # @permission_classes([IsAdminUser, AdminRequired])
+    @permission_classes([IsAdminUser, AdminRequired])
     def list(self, request):
         serializer = TransactionSerializer(self.queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -45,3 +49,69 @@ class TransactionListRetrieveViewSet(viewsets.ViewSet):
         transaction = get_object_or_404(self.queryset, pk=pk)
         serializer = TransactionSerializer(transaction, many=False)
         return Response(serializer.data)
+
+
+#!TransactionCreateViewSet
+class TransactionCreateViewSet(viewsets.ViewSet):
+
+    """
+    A Viewset for create Transaction
+    """
+
+    queryset = Transaction.objects.get_is_complete()
+    serializer_class = TransactionSerializer
+
+    @permission_classes([IsAdminUser, IsAuthenticated, AdminRequired, ProfileRequired])
+    def create(self, request):
+        serializer = TransactionSerializer(data=request.data)
+        if serializer.is_valid():
+            transaction_obj = Transaction.objects.create(**serializer.validated_data)
+            return Response(
+                TransactionSerializer(transaction_obj).data,
+                status=status.HTTP_201_CREATED,
+            )
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+#!BlockListRetrieveViewSet
+class BlockListRetrieveViewSet(viewsets.ViewSet):
+
+    """
+    A Viewset for viewing all Block
+    """
+
+    queryset = Block.objects.get_is_complete()
+    serializer_class = BlockSerializer
+    lookup_field = "pk"
+
+    @permission_classes([IsAdminUser, AdminRequired])
+    def list(self, request):
+        serializer = BlockSerializer(self.queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @permission_classes([IsAdminUser, AdminRequired])
+    def retrieve(self, request, pk=None):
+        transaction = get_object_or_404(self.queryset, pk=pk)
+        serializer = BlockSerializer(transaction, many=False)
+        return Response(serializer.data)
+
+
+#!BlockCreateViewSet
+class BlockCreateViewSet(viewsets.ViewSet):
+    """
+    A Viewset for create Block
+    """
+
+    queryset = Block.objects.get_is_complete()
+    serializer_class = BlockSerializer
+
+    def create(self, request):
+        serializer = BlockSerializer(data=request.data)
+        if serializer.is_valid():
+            block_obj = Block.objects.create(**serializer.validated_data)
+            return Response(
+                BlockSerializer(block_obj).data, status=status.HTTP_201_CREATED
+            )
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
