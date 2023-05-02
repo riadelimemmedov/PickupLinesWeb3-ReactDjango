0d12a661873630fbfceaaeb298e63bfeea770d88
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-05-02 10:44:13 +0400

    Is registered TransactionFactory

---
+++ backend/config/tests/conftest.py
@@ -0,0 +1,16 @@
+# RestFramework Function And Module
+from rest_framework.test import APIClient
+
+
+# Pytest
+import pytest
+from pytest_factoryboy import register
+
+
+# Factories
+from .factories import TransactionFactory
+
+
+# *Register TestModelFactory
+# if you want call this Factory in the test file you need to declare  bottom_line format = category_factory
+register(TransactionFactory)
