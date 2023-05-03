cfca504e797fcb8c15afcca147e334ca158216a0
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-05-04 00:20:44 +0400

    Is registered BlockFactory method for testing models

--- backend/config/tests/conftest.py
+++ backend/config/tests/conftest.py
@@ -8,9 +8,10 @@ from pytest_factoryboy import register
 
 
 # Factories
-from .factories import TransactionFactory
+from .factories import TransactionFactory, BlockFactory
 
 
 # *Register TestModelFactory
 # if you want call this Factory in the test file you need to declare  bottom_line format = category_factory
 register(TransactionFactory)
+register(BlockFactory)
