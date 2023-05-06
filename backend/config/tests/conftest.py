56f7a9de08a6d99eb75cccf2e027218ca0d00e26
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-05-06 10:49:15 +0400

    Is added api_client for sending request fake api url

--- backend/config/tests/conftest.py
+++ backend/config/tests/conftest.py
@@ -15,3 +15,9 @@ from .factories import TransactionFactory, BlockFactory
 # if you want call this Factory in the test file you need to declare  bottom_line format = category_factory
 register(TransactionFactory)
 register(BlockFactory)
+
+
+#!api_client
+@pytest.fixture  # Fixtures are functions,which will run before each test function to which it is applied.Fixtures are used to feed some data to the tests such as database connections,URLs to test and some sort of input data.
+def api_client():
+    return APIClient
