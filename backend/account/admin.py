69e323382d20d08821a14b19699655fc6587d8f0
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-05-09 09:51:08 +0400

    Is created AccountAdminForm for validate metamask address format true or not

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -4,6 +4,10 @@ from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
 from django.utils.html import format_html
 
+from django import forms
+import re
+from django.core.exceptions import ValidationError
+
 
 # Module and Serializers
 from .models import Account
@@ -11,10 +15,29 @@ from .models import Account
 # Register your models here.
 
 
+# *AccountAdminForm
+class AccountAdminForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = "__all__"
+
+    def clean_metamask_address(self):
+        metamask_address = self.cleaned_data["metamask_address"]
+        pattern = re.compile("^0x[a-fA-F0-9]{40}$")
+        is_match = bool(pattern.match(f"{metamask_address}"))
+        if is_match == False:
+            raise ValidationError("Please enter a valida metamask address")
+        return metamask_address
+
+
+####################################################################################################################################
+
 #!AccountAdmin
 @admin.register(Account)
 class AccountAdmin(UserAdmin):
     # thumbnail
+    form = AccountAdminForm
+
     def thumbnail(self, object):
         if object.account_picture:
             return format_html(
@@ -38,6 +61,8 @@ class AccountAdmin(UserAdmin):
                 "fields": (
                     "first_name",
                     "last_name",
+                    "username",
+                    "metamask_address",
                     "account_picture",
                     "registration_number",
                 )
@@ -84,6 +109,7 @@ class AccountAdmin(UserAdmin):
         "last_login",
         "role",
         "thumbnail",
+        "metamask_address",
     ]
     list_filter = [
         "is_staff",
