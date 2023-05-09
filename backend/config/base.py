7f74cb18ce8bb232d8c566e1ce58e00193cb3f75
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-05-09 09:51:43 +0400

    Changed djoser settings in base file

--- backend/config/base.py
+++ backend/config/base.py
@@ -155,6 +155,9 @@ DJOSER = {
 }
 
 
+# http://127.0.0.1:8000/api/v1/users/create/ => For register user
+# http://127.0.0.1:8000/api/v1/token/login => For login user
+
 #!Root UrlConf
 ROOT_URLCONF = "config.urls"
 
