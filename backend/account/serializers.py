c7290823ff013326330e095e30c09b5d559b8092
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-05-09 09:51:30 +0400

    Is created CustomTokenSerializer for Djoser authentication

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -4,20 +4,23 @@ from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
 
+#!CustomTokenSerializer
 class CustomTokenSerializer(TokenCreateSerializer):
     metamask_address = serializers.CharField()
-    
-    def validate(self,attrs):
-        metamask_address = attrs.get('metamask_address')
-        if metamask_address:
-            user = authenticate(metamask_address=metamask_address)
-            
+    username = serializers.CharField()
+
+    def validate(self, attrs):
+        metamask_address = attrs.get("metamask_address")
+        username = attrs.get("username")
+
+        if metamask_address and username:
+            user = authenticate(metamask_address=metamask_address, username=username)
             if not user:
-                msg = _('Unable to log in with provided credentials')
-                raise serializers.ValidationError(msg,code='authorization')
+                msg = _("Unable to log in with provided credentials")
+                raise serializers.ValidationError(msg, code="authorization")
         else:
             msg = _("Must include 'metamask_address'. ")
-            raise serializers.ValidationError(msg,code='authorization')
-        
-        attrs['user'] = user
+            raise serializers.ValidationError(msg, code="authorization")
+
+        attrs["user"] = user
         return attrs
