2ab1f78aadbc37a12c2ac12e7402befb6b1e8bc0
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-05-12 08:25:04 +0400

    Created CustomTokenCreateSerializer for CustomTokenSerializer class

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from djoser.views import TokenCreateView
+from .serializers import CustomTokenSerializer
+
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
 
 # Create your views here.
+
+
+#!CustomTokenCreateSerializer
+class CustomTokenCreateSerializer(TokenCreateView):
+    serializer_class = CustomTokenSerializer
