ccd4c6893ecfb17262c32f3458b7cd7d542078be
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-05-12 08:24:47 +0400

    Is created CustomTokenSerializer for customize Token Authentication process

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -1,26 +1,42 @@
-from djoser.serializers import TokenCreateSerializer
+# Django
+from django.shortcuts import get_object_or_404
 from django.contrib.auth import authenticate
 from django.utils.translation import gettext_lazy as _
+from django.db.models import Q
+
+# Serializers and Models
+from account.models import Account
+
+
+# Django Rest
+from rest_framework.authtoken.models import Token
 from rest_framework import serializers
+from rest_framework.response import Response
+
+
+# Third Party
+from djoser.serializers import TokenCreateSerializer
 
 
 #!CustomTokenSerializer
 class CustomTokenSerializer(TokenCreateSerializer):
-    metamask_address = serializers.CharField()
-    username = serializers.CharField()
+    # validate
+    def validate(self, data):
+        email = data.get("email")
+        username = data.get("username")
+        password = data.get("password")
+        metamask_address = data.get("metamask_address")
 
-    def validate(self, attrs):
-        metamask_address = attrs.get("metamask_address")
-        username = attrs.get("username")
+        if not email:
+            raise serializers.ValidationError("Email is required")
 
-        if metamask_address and username:
-            user = authenticate(metamask_address=metamask_address, username=username)
-            if not user:
-                msg = _("Unable to log in with provided credentials")
-                raise serializers.ValidationError(msg, code="authorization")
-        else:
-            msg = _("Must include 'metamask_address'. ")
-            raise serializers.ValidationError(msg, code="authorization")
+        if not username:
+            raise serializers.ValidationError("Username is required")
 
-        attrs["user"] = user
-        return attrs
+        if not password:
+            raise serializers.ValidationError("Password is required")
+
+        if not metamask_address:
+            raise serializers.ValidationError("Metamask Address is required")
+
+        return data
